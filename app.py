bd8766fe777f162b1bab00cf848b4df71554754e
Antonio Mario Pio <antonio.frioli1@gmail.com>
2023-10-16 01:33:12 -0300

    db change

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from models.variantes import db as variantes_db
 # Inicialização do aplicativo Flask
 app = Flask(__name__)
 
-# Configuração do banco de dados SQLite para pacientes
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///127.0.0.1:3306'
+# Configuração do banco de dados MYSQL para pacientes
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///127.0.0.1:3306/db_einstein'
 
 pacientes_db.init_app(app)
 
